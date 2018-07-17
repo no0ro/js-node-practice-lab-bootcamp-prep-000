173b5a8ca50d10898d7346bad4cf54bc4fb732e9
Noelle Rolwes <nrolwes@gmail.com>
2018-07-17 06:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Noelle"
-var height = "74"
+var height = 70
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
+console.log("Height:"+1)
 console.log(height)
